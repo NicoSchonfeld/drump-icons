d5e86c70c6653a7bc5332ff782c95274443f52e6
Nicolas Schonfeld <nicolas.schonfeld@25watts.com.ar>
2023-11-13 22:44:43 -0300

    5.1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,79 +1,16 @@
-"use client";
-
-import React, { useState, useEffect, type MouseEvent } from "react";
+import React from "react";
 import Image from "next/image";
 
 import IconSectionComponent from "@/components/IconSectionComponent";
 
 import Luz from "../../public/luz.svg";
 
-import { motion } from "framer-motion";
-
 const App: React.FC = () => {
-  const style =
-    "w-4 h-4 rounded-full bg-white fixed top-0 left-0 pointer-events-none z-10";
-
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0,
-  });
-
-  const [cursorVariant, setCursorVariant] = useState("default");
-
-  useEffect(() => {
-    const mouseMove = (e: MouseEvent<HTMLDivElement>) => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY,
-      });
-    };
-
-    if (typeof window !== "undefined") {
-      window?.addEventListener("mousemove", mouseMove);
-    }
-
-    return () => {
-      if (typeof window !== "undefined") {
-        window?.removeEventListener("mousemove", mouseMove);
-      }
-    };
-  }, []);
-
-  const variants: any = {
-    default: {
-      x: mousePosition.x - 8,
-      y: mousePosition.y - 8,
-    },
-
-    text: {
-      height: 150,
-      width: 150,
-      x: mousePosition.x - 70,
-      y: mousePosition.y - 70,
-      backgroundColor: "white",
-      mixBlendMode: "difference",
-    },
-  };
-
-  const textEnter = () => setCursorVariant("text");
-  const textLeave = () => setCursorVariant("default");
-
   return (
     <main className="relative w-full h-auto bg-indigo-950">
-      <motion.div
-        variants={variants}
-        initial={false}
-        animate={cursorVariant}
-        className={style}
-      ></motion.div>
-
       <div className="w-full h-full container mx-auto p-20">
         <section className="w-full h-full flex flex-col items-center justify-center gap-5 p-20">
-          <h1
-            onMouseEnter={textEnter}
-            onMouseLeave={textLeave}
-            className="bg-clip-text text-transparent bg-gradient-to-t from-indigo-200  to-white z-[1] text-8xl font-bold"
-          >
+          <h1 className="bg-clip-text text-transparent bg-gradient-to-t from-indigo-200  to-white z-[1] text-8xl font-bold">
             Drump Icons
           </h1>
           <p className="text-center text-gray-600 z-[1]">
